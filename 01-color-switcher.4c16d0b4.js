3917a12719a2c259f22f2a0ce7e89ff70678d186
NicaLaur <142085072+NicaLaur@users.noreply.github.com>
2024-02-04 20:35:50 +0000

    Deploying to gh-pages from @ NicaLaur/goit-js-hw-09@b7bab150f6d35fdce4e374efb14ce10153e6414c 🚀

---
+++ 01-color-switcher.4c16d0b4.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.4c16d0b4.js.map
